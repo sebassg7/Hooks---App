8ca8cb0f8cbb4a9da741a31ddf4c8b504e5211c1
Sebastian L Moran <sbssing7@gmail.com>
2023-10-17 11:56:12 -0500

    useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -3,12 +3,38 @@ import { useFetch } from "../hooks/useFetch";
 export const MultipleCustomHooks = () => {
 
     const {data,isLoading,hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
-    console.log({data,isLoading,hasError});
+    
+    // const{author, quote} = false; "El valor de author y quote son undefined";
+    console.log(!!data);
+    const {author, quote} = !!data && data[0];
+    
+    
+      
 
   return (
     <>
         <h1>BreakingBad Quotes</h1>
         <hr />
+
+        {
+            (isLoading)
+                ? (
+                    <div className="alert alert-info text-center">
+                        Loading...
+                    </div>
+                )
+                :(
+                    <blockquote className="blockquote text-end">
+                        <p className="mb-1"> {quote}</p>
+                        <footer className="blockquote-footer">{author}</footer>
+                    </blockquote>
+                )
+        } 
+
+        <button className="btn btn-primary">
+            Nexy quote
+        </button>
+        
     </>
   )
 };
