d91a79826accd0f89eb40d75a633f588993aacf0
Sebastian L Moran <sbssing7@gmail.com>
2023-10-17 18:28:32 -0500

    useLayoutEffect

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,32 @@
+import { useCounter, useFetch } from "../hooks/index";
+import { LoadingQuote, Quote } from "../03-examples/index";
+
+export const Layout = () => {
+const {counter, increment} = useCounter();
+    const {data,isLoading,hasError} = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+    
+    // const{author, quote} = false; "El valor de author y quote son undefined";
+
+    const {author, quote} = !!data && data[0];
+
+  return (
+    <>
+        <h1>BreakingBad Quotes</h1>
+        <hr />
+
+        {
+            (isLoading)
+                ?<LoadingQuote/>
+                :<Quote author={author} quote={quote}/>      
+        } 
+
+        <button 
+        onClick={() => increment()} 
+        disabled ={isLoading}
+        className="btn btn-primary">
+            Nexy quote
+        </button>
+        
+    </>
+  )
+};
--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,14 @@ import ReactDOM from 'react-dom/client';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
 import './index.css'
-
-
-
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks/>
+    <Layout/>
   </React.StrictMode>,
 )
