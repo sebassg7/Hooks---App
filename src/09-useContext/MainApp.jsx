db3a4aa30f03d3d41c22cc7de24926cd998dcef9
Sebastian L Moran <sbssing7@gmail.com>
2023-10-25 11:31:19 -0500

    useContext - Configurar Router en React

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,30 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { AboutPage } from "./AboutPage";
+// import { HomePage, AboutPage, LoginPage } from "./index";
+
+
+
+export const MainApp = () => {
+  return (
+    <>
+        <h1>MainApp</h1>
+        <hr />
+
+        <Routes>
+            <Route path="/" element={<HomePage/>}/> 
+            <Route path="about" element={<AboutPage/>}/> 
+            <Route path="login" element={<LoginPage/>}/> 
+
+            {/* <Route path="/*" element={<LoginPage/>}/>  <= Queda con la dirección equivocada */}
+
+            <Route path="/*" element={<Navigate to="about"/>}/> 
+
+
+
+        </Routes>
+       
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+
+// import { BrowserRouter } from "react-router-dom";
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
@@ -13,13 +16,26 @@ import ReactDOM from 'react-dom/client';
 // import { CallBackHook } from './06-memos/CallBackHook';
 // import { Padre } from './07-tarea-memo/Padre';
 // import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+// import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 import './index.css';
 
-
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MainApp/>,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp/>
-  // </React.StrictMode>,
+ 
+  <BrowserRouter>
+  
+  {/* <React.StrictMode>  */}
+      <MainApp />
+  {/* </React.StrictMode> */}
+
+  </BrowserRouter>
+    
+  
 )
